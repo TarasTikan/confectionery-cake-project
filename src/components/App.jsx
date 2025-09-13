1c0d1b2c9f4348446e52970fb09292afd0f19f7f
Taras Tikan <109157753+TarasTikan@users.noreply.github.com>
2025-09-13 19:27:15 +0300

    add img to menu

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,14 @@
 import '../App.css';
 import { SideBar } from './header/header';
 import {Hero} from './hero/hero'
+import {Menu} from './menu/menu'
 function App() {
   return (
     <>
     <SideBar/>
     <main>
       <Hero/>
+      <Menu/>
     </main>
     </>
   );
---
+++ src/components/menu/menu.jsx
@@ -0,0 +1,32 @@
+
+
+export const Menu = () => {
+  return (
+    <section>
+      <div>
+        <ul>
+          <li>
+            <a href="/">
+              <img src="/" />
+            </a>
+          </li>
+            <li>
+            <a href="/">
+              <img src="/" />
+            </a>
+          </li>
+            <li>
+            <a href="/">
+              <img src="/" />
+            </a>
+          </li>
+            <li>
+            <a href="/">
+              <img src="/" />
+            </a>
+          </li>
+        </ul>
+      </div>
+    </section>
+  );
+};
