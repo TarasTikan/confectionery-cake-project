a06e16935373738fc7ec7d9ac9b703ce142d1b4b
Taras Tikan <109157753+TarasTikan@users.noreply.github.com>
2026-01-02 17:32:12 +0200

    add debug modalMobNav

--- src/components/MobMenu/MobMenu.jsx
+++ src/components/MobMenu/MobMenu.jsx
@@ -9,52 +9,60 @@ import {
   Backdrop,
   NavLinkAuth,
   WrapperAuth,
-  Separator
+  Separator,
 } from "./MobMenu.styled";
 import { getCartItems } from "../../redux/cart/selectors";
+import { selectAuthUser } from "../../redux/auth/selectors";
 
 export const MobMenu = ({ clouseMobMenu, open }) => {
   const cart = useSelector(getCartItems);
+  const isAuth = useSelector(selectAuthUser);
   const handleClouse = () => {
     clouseMobMenu();
   };
+  console.log(isAuth);
   return (
     <Backdrop open={open} onClick={handleClouse}>
-    <MobMenuSection open={open}>
-      <MobMenuContainer>
-        <div>
-          <BtnHeaderClouse type="button" onClick={handleClouse}>
-            x
-          </BtnHeaderClouse>
+      <MobMenuSection open={open}>
+        <MobMenuContainer>
+          <div>
+            <BtnHeaderClouse type="button" onClick={handleClouse}>
+              x
+            </BtnHeaderClouse>
 
-          <nav>
-            <MobMenuList>
-              <li>
-                <NavLink to="/" onClick={handleClouse}>
-                  Головна
-                </NavLink>
-              </li>
-              <li>
-                <NavLink to="/menu" onClick={handleClouse}>
-                  Меню
-                </NavLink>
-              </li>
-              <li>
-                <NavLink to="/about" onClick={handleClouse}>
-                  Про нас
-                </NavLink>
-              </li>
-            </MobMenuList>
-          </nav> 
-        </div>
-        <WrapperAuth>
+            <nav>
+              <MobMenuList>
+                <li>
+                  <NavLink to="/" onClick={handleClouse}>
+                    Головна
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink to="/menu" onClick={handleClouse}>
+                    Меню
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink to="/about" onClick={handleClouse}>
+                    Про нас
+                  </NavLink>
+                </li>
+              </MobMenuList>
+            </nav>
+          </div>
+          {!isAuth && (
+            <WrapperAuth>
               <NavLinkAuth to="/register">Зареєструватися</NavLinkAuth>
               <Separator>|</Separator>
-                  <NavLinkAuth to="/login">Увійти</NavLinkAuth>  
-                  </WrapperAuth>
-        <MobMenuBtn to={cart.length === 0 ? "/menu" : "/order"}>Замовити зараз</MobMenuBtn>
-      </MobMenuContainer>
-    </MobMenuSection>
+              <NavLinkAuth to="/login">Увійти</NavLinkAuth>
+            </WrapperAuth>
+          )}
+
+          <MobMenuBtn to={cart.length === 0 ? "/menu" : "/order"}>
+            Замовити зараз
+          </MobMenuBtn>
+        </MobMenuContainer>
+      </MobMenuSection>
     </Backdrop>
   );
 };
